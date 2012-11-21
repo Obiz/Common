14c3d2af5bd700cd94053168d9a73faa0d19ef26
drgomesp <drgomesp@gmail.com>
2012-11-21 09:05:40 -0200

    Major refactoring to make things a lot simpler

---
+++ src/Obiz/Common/DependencyInjection/Exception/MissingDependencyException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Obiz\Common\DependencyInjection\Exception;
+
+use \Exception;
+
+/**
+ * A missing dependency exception.
+ *
+ * @author Daniel Ribeiro <daniel@obiz.com.br>
+ */
+class MissingDependencyException extends Exception
+{}
--- src/Obiz/Common/Entity.php
+++ src/Obiz/Common/Entity.php
@@ -2,5 +2,15 @@
 
 namespace Obiz\Common;
 
+/**
+ * Defines an Entity.
+ *
+ * @author Daniel Ribeiro <daniel@obiz.com.br>
+ */
 interface Entity
-{}
+{
+    /**
+     * @return int
+     */
+    public function getId();
+}
