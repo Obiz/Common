a737898343063f43e6a5e4c5dc21df73a41da21e
drgomesp <drgomesp@gmail.com>
2013-01-06 14:00:15 -0200

    Deleted Query interface and created Database interface

---
+++ src/Obiz/Common/Persistence/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Obiz\Common\Persistence;
+
+/**
+ * Represents a database object.
+ *
+ * @author Daniel Ribeiro <daniel@obiz.com.br>
+ */
+interface Database
+{
+    /**
+     * Executes a query.
+     *
+     * @param string $query
+     */
+    public function execute($query);
+}
